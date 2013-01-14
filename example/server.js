97dd829ad1df81216a741b86782b0449f1f5905b
Joshua Gross <joshua.gross@gmail.com>
2013-01-13 22:51:29 -0600

    code formatting

--- example/server.js
+++ example/server.js
@@ -11,16 +11,18 @@ var http = require('http')
 serverswap.readyFor(':8080', function () {
   var server = http.createServer(function (req, res) {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end("Hello World!\n");
+    setTimeout(function () {
+      res.end("Hello World!\n");
+    },Math.random()*4000)
   }).listen(8080);
 
   console.log('Listening on port 8080');
 
   serverswap.onTakedown(':8080', function () {
-    // Stop taking new connections on port 80
+    //Stop taking new connections on port 80
     console.log('Took down port 8080');
     server.close();
-    // Give existing requests 5 seconds to finish processing
+     //Give existing requests 5 seconds to finish processing
     setTimeout(function () {
       process.exit(0);
     }, 5*1000);
