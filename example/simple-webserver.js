90c6b418665fd19a2435d279b5b7cbc33cfc43fa
Joshua Gross <joshua.gross@gmail.com>
2013-02-21 21:58:44 -0600

    Fixed example servers to use reconnect-on-error strategy

--- example/simple-webserver.js
+++ example/simple-webserver.js
@@ -14,7 +14,9 @@ serverswap.readyFor(':8080', function () {
     setTimeout(function () {
       res.end("Hello World!\n");
     },Math.random()*0)
-  }).listen(8080, function () {
+  });
+
+  var onListen = function () {
     console.log('Example server [',process.pid,'] Listening on port 8080');
 
     serverswap.serverUp(); // optional otherwise, but necessary for "deploy" mode
@@ -24,12 +26,28 @@ serverswap.readyFor(':8080', function () {
       console.log('Example server [',process.pid,'] Taking down port 8080');
       server.close(function () {
         console.log('Example server [',process.pid,'] Took down port 8080');
+
         done();
+
+         //Give existing requests 5 seconds to finish processing
+        setTimeout(function () {
+          process.exit(0);
+        }, 5*1000);
       });
-       //Give existing requests 5 seconds to finish processing
-      setTimeout(function () {
-        process.exit(0);
-      }, 5*1000);
     });
-  });
+  };
+
+  var attemptListen = function () {
+    server.listen(8080);
+  };
+
+  server.on('listening', onListen);
+
+  server.on('error', function (e) {
+    if (e.code === 'EADDRINUSE') {
+      process.nextTick(attemptListen);
+    }
+  })
+
+  attemptListen();
 })
